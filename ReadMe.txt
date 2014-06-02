a650fd3559132a88cd2bfb8c48179d291e270323
Sathya Laufer <sathya@laufers.net>
2014-06-02 12:53:13 +0200

    SSL support

---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+Usage:
+
+Require the file "Client.php" somewhere at the top of your PHP file:
+
+	require_once("Client.php");
+
+Create a new instance of the client with:
+
+	With SSL:
+	$Client = new \XMLRPC\Client(HOSTNAME, PORT, true);
+
+	Without SSL:
+	$Client = new \XMLRPC\Client(HOSTNAME, PORT, false);
+
+SSL options:
+	To enable/disable certificate verification:
+		$Client->setSSLVerifyPeer(true);
+	
+	To set the path to your CA certificate (needed when certificate verification is enabled):
+		$Client->setCAFile("/path/to/ca.crt");
+		
+	To set your Homegear username and password:
+		$Client->setUsername(USERNAME);
+		$Client->setUsername(PASSWORD);
+	
+And then invoke XML RPC methods with:
+
+	$Client->send("METHODNAME", array(PARAMETERS));
+
+See Test.php for an example.
---
+++ Test.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+$clientPath = (PHP_OS == "WINNT") ? "HM-XMLRPC-Client\\" : "HM-XMLRPC-Client/";
+require_once($clientPath."Client.php");
+
+$host = "homegear";
+$port = 2003;
+$ssl = true;
+$username = "homegear";
+$password = "homegear";
+
+$Client = new \XMLRPC\Client($host, $port, true);
+$Client->setSSLVerifyPeer(false);
+
+//!!! You should create a signed certificate and then enable "verify peer" !!!
+//$Client->setSSLVerifyPeer(true);
+//$Client->setCAFile("/path/to/ca.crt");
+
+$Client->setUsername($username);
+$Client->setPassword($password);
+
+print_r($Client->send("listDevices", array()));
+//print_r($Client->send("system.methodHelp", array("getLinks")));
+//print_r($Client->send("getDeviceDescription", array(23, 1)));
+//print_r($Client->send("getLinkInfo", array(23, 1, 37, 3)));
+//print_r($Client->send("getMetadata", array("54:2")));
+//print_r($Client->send("getParamset", array(12, -1, "MASTER")));
+//print_r($Client->send("getParamset", array(23, 1, 37, 3)));
+//print_r($Client->send("getParamsetDescription", array(23, 1, "LINK")));
+//print_r($Client->send("getParamsetId", array(23, 1, 37, 3)));
+//print_r($Client->send("getServiceMessages", array()));
+//print_r($Client->send("getLinkPeers", array(27, 1)));
+//print_r($Client->send("getLinks", array()));
+//print_r($Client->send("getLinks", array(27, 1, 31)));
+//print_r($Client->send("putParamset", array(54, 1, "MASTER", array("EVENT_DELAYTIME" => 0.0))));
+//print_r($Client->send("putParamset", array(63, 18, "MASTER", array("MESSAGE_SHOW_TIME" => 0.0))));
+//print_r($Client->send("setTeam", array(12, 1)));
+//print_r($Client->send("setTeam", array(12, 1, 0x80000002, 1)));
+//print_r($Client->send("setValue", array(15, 1, "ON_TIME", 787487.3)));
+//print_r($Client->send("setValue", array(15, 1, "STATE", true)));
+//print_r($Client->send("setValue", array(19, 1, "VALVE_STATE", 0)));
+//print_r($Client->send("setValue", array(21, 2, "SETPOINT", 16.0)));
+//print_r($Client->send("setValue", array(62, 18, "TEXT", "Hi")));
+//print_r($Client->send("setValue", array(63, 18, "SUBMIT", true)));
+?>
+
